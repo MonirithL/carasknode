0377509c156dad461b435e96a2a6a43cb86cce81
MonirithL <lengmonirith@gmail.com>
2025-10-19 17:46:15 +0700

    added fallback

--- index.js
+++ index.js
@@ -39,6 +39,9 @@ app.use("/api/progress", progressRouter);
 const PORT = 3000;
 
 app.use(express.static(path.join(__dirname, "dist")));
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "dist", "index.html"));
+});
 app.get('/api/', (req, res, ) => {
   res.send("First message before hello b")
 })
